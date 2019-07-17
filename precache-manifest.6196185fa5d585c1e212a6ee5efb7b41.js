5603e5defbccd1578c447d120fd6fca0ea15d867
Oleksandr Hutsulyak <kamiklviv@gmail.com>
2019-07-17 15:18:27 +0300

    Updates

---
+++ precache-manifest.6196185fa5d585c1e212a6ee5efb7b41.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d522803b60ca68d3113ea3da5329da7f",
+    "url": "/cocktail-finder/index.html"
+  },
+  {
+    "revision": "87a2ce607b2e18dc5369",
+    "url": "/cocktail-finder/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "66cdff376d8f9c1bb199",
+    "url": "/cocktail-finder/static/js/2.f8277a14.chunk.js"
+  },
+  {
+    "revision": "87a2ce607b2e18dc5369",
+    "url": "/cocktail-finder/static/js/main.bb1c995d.chunk.js"
+  },
+  {
+    "revision": "924573bdc8df2cc3bfe7",
+    "url": "/cocktail-finder/static/js/runtime~main.c7ba4e92.js"
+  }
+]);
